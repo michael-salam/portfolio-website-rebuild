b83fc7584d1f18e41894178edd28987941c6be6b
Michael Salam <michaeldimimu@gmail.com>
2022-09-01 20:25:44 +0100

    Switched positions of services and projects sections

--- src/App.js
+++ src/App.js
@@ -2,8 +2,8 @@ import "./App.scss";
 
 import Navbar from "./components/Navbar/Navbar";
 import Header from "./components/Header/Header";
-import Services from "./components/Services/Services";
 import Projects from "./components/Projects/Projects";
+import Services from "./components/Services/Services";
 import AboutMe from "./components/AboutMe/AboutMe";
 import GetInTouch from "./components/GetInTouch/GetInTouch";
 
@@ -12,8 +12,8 @@ function App() {
     <>
       <Navbar />
       <Header />
-      <Services />
       <Projects />
+      <Services />
       <AboutMe />
       <GetInTouch />
     </>
